8d75613b736aebe114ce9d1bb26dc8b7516824ce
Donghyun Go <godong9@gmail.com>
2016-07-30 20:51:24 +0900

    Update board

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -2,11 +2,21 @@ $('document').ready(function() {
   var locId = location.search.replace('?loc_id=', '');
   var boardItems = [];
 
-  console.log(locId);
   function draw() {
-    HttpUtil.getData('locations/'+locId+'/comments', {}, function(err, data) {
-      console.log(data);
+    HttpUtil.getData('locations/'+locId+'/comments', {}, function(data) {
+      if (data && data.length > 0) {
+        boardItems = data;
+        for (var i = 0; i < boardItems.length; i++) {
+          drawBoardItem(boardItems[i]);
+        }
+        bindClickEvent(boardItems[i]);
+      }
     });
+  }
+
+  function drawBoardItem(boardItem) {
+    var targetItem = $('.board-item[data-position=' + boardItem.index_x.toString() + boardItem.index_y.toString() + ']')
+    targetItem.children('textarea').text(boardItem.content);
   }
 
   function bindClickEvent() {
@@ -15,17 +25,15 @@ $('document').ready(function() {
       var itemPosition = $(this).data('position');
 
       console.log(itemText);
-      if (!itemText) {
-        //TODO: 작성하기용 모달 띄우기!
-        alert("작성");
-      } else {
+      if (itemText) {
         //TODO: 댓글 모달 띄우기!
         alert("댓글");
+      } else {
+        //TODO: 작성하기용 모달 띄우기!
+        alert("작성");
       }
     });
   }
 
   draw();
-  bindClickEvent();
-
 });
