db5ebbb08cd5b003c192f21e4cedd72e6393f2d6
Donghyun Go <godong9@gmail.com>
2016-07-31 01:30:53 +0900

    Update main

--- tts-test.js
+++ tts-test.js
@@ -15,7 +15,7 @@ var options = {
   },
   body: querystring.stringify({
     speaker: 'mijin',
-    text: '아 똥 싸고 싶다',
+    text: '저기 저 뜀틀이 내가 뛸 뜀틀인가 내가 안 뛸 뜀틀인가 내가 그린 기린 그림은 목이 긴 기린 그림인가 목이 안긴 기린 그림인가 네가 그린 기린 그림은 못 그린 기린 그림이고 내가 그린 기린 그림은 잘 그린 기린 그림이다',
     speed: 0
   })
 };
