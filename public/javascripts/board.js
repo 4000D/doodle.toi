f6291c9197e219a265e530ad4e4b613e771775c0
Donghyun Go <godong9@gmail.com>
2016-07-30 18:58:31 +0900

    Update loading

--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -1,3 +1,11 @@
 $('document').ready(function() {
 
+  function bindClickEvent() {
+    $('.board-item').click(function() {
+      alert("CLICK");
+    });
+  }
+
+  bindClickEvent();
+
 });
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -19,29 +19,13 @@ $( document ).ready(function() {
     $('.login-box').show('slow');
   }
 
-  function changeToLoadingPage() {
-    $('#main_page_container').hide();
-    $('#loading_page_container').show();
-
-    if (!cur_latitude || !cur_longitude) { //위치 못가져왔을 때 실패 이미지 표시
-      $('#get_location_success_image').hide();
-      $('#get_location_fail_image').show();
-    } else {
-      setTimeout(function() {
-        location.href = "/board?lat="+cur_latitude+"&lng="+cur_longitude;
-      }, 3000);
-    }
-  }
-
   function bindClickEvent() {
     $('#naver_login_btn').click(function() {
-      alert("네이버 로그인 처리");
-      changeToLoadingPage();
+      location.href = "/auth/naver";
     });
 
     $('#kakao_login_btn').click(function() {
-      alert("카카오 로그인 처리");
-      changeToLoadingPage();
+      location.href = "/auth/kakao";
     });
   }
 
@@ -52,61 +36,5 @@ $( document ).ready(function() {
     changeToLoginPage();
   }, 2000);
 
-
-  var
-    result
-  , cur_latitude
-  , cur_longitude
-  , loc_id;   
-
-  function successCallback(position) {
-    cur_latitude = position.coords.latitude;
-    cur_longitude = position.coords.longitude;
-
-    // TODO: 그나마 좀 가까운 Location만 가져오기
-    $.ajax('/locations', {
-      type: 'GET',
-      async: true
-    }).then( result => {
-      var min_dist = 20000000;
-
-      result.forEach( (loc) => {
-        var dist = distance(cur_latitude, cur_longitude, loc.latitude, loc.longitude);
-        console.log(`distance(${cur_latitude}, ${cur_longitude}, ${loc.latitude}, ${loc.longitude}) : ${dist}, ${loc._id}`);
-        if (dist < min_dist) {
-          min_dist = dist;
-          loc_id = loc._id;
-        }
-      });
-
-      console.log(`closest: ${cur_latitude}, ${cur_longitude} ~ ${loc_id}`);
-    });
-  }
-
-
-  function errorCallback(error) {
-    alert(error.message);
-  }
-
-  // need authrization
-  function load(){ 
-    if ( navigator.geolocation ) {
-      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-    } else {
-      alert("geolocation not supported");
-    }
-  }
-
-  function distance(lat1, lon1, lat2, lon2) {
-    var p = 0.017453292519943295;    // Math.PI / 180
-    var c = Math.cos;
-    var a = 0.5 - c((lat2 - lat1) * p)/2 + 
-            c(lat1 * p) * c(lat2 * p) * 
-            (1 - c((lon2 - lon1) * p))/2;
-
-    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
-  }
-
-  load();
 });
 
